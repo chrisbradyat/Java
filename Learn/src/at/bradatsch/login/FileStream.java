ef420d712b0b7adbdc1c3019ed8cc4569a9f3421
Christian Bradatsch <christian.bradatsch@icloud.com>
2015-09-02 14:08:17 +0200

    ok

---
+++ Learn/src/at/bradatsch/login/FileStream.java
@@ -0,0 +1,8 @@
+package at.bradatsch.login;
+
+public class FileStream {
+	
+	String test;
+	
+
+}
---
+++ Learn/src/at/bradatsch/login/Main.java
@@ -0,0 +1,11 @@
+package at.bradatsch.login;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hi");
+		
+	}
+
+}
